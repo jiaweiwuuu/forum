24e8e2d6e519f0f59866fb03f1197a8d05bf134f
姚志远 <yaozhiyuan@192.168.5.54>
2020-10-21 14:59:23 +0800

    fix

--- forum.sql
+++ forum.sql
@@ -19,116 +19,24 @@
 -- Table structure for table `comment`
 --
 
-DROP TABLE IF EXISTS `comment`;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `comment` (
+CREATE TABLE `user` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `content` text,
-  `entity_id` int(11) NOT NULL,
-  `entity_type` int(11) NOT NULL,
-  `create_at` datetime DEFAULT NULL,
-  `user_id` int(11) unsigned DEFAULT NULL,
+  `name` varchar(64) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `salt` varchar(32) NOT NULL,
+  `head_url` varchar(256) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `comment_user_id_fk` (`user_id`),
-  CONSTRAINT `comment_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  KEY `index2` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `comment`
---
-
-LOCK TABLES `comment` WRITE;
-/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
-INSERT INTO `comment` VALUES (24,'this is a comment 11',4,0,'2020-10-18 23:40:01',17),(25,'this is a comment 11',5,0,'2020-10-19 18:00:53',21);
-/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `contact_list`
---
-
-DROP TABLE IF EXISTS `contact_list`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `contact_list` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `conversation_id` varchar(1238) NOT NULL,
-  `create_at` datetime NOT NULL,
-  `update_at` datetime NOT NULL,
-  `latest_message_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `date_index` (`create_at`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `contact_list`
---
-
-LOCK TABLES `contact_list` WRITE;
-/*!40000 ALTER TABLE `contact_list` DISABLE KEYS */;
-INSERT INTO `contact_list` VALUES (1,'17-19','2020-10-20 03:28:26','2020-10-20 03:29:12',2),(2,'17-20','2020-10-20 03:47:30','2020-10-20 03:47:30',3),(3,'17-22','2020-10-20 03:49:56','2020-10-20 03:49:56',4);
-/*!40000 ALTER TABLE `contact_list` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `feed`
---
-
-DROP TABLE IF EXISTS `feed`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `feed` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `type` int(11) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `create_at` datetime DEFAULT NULL,
-  `data` tinytext,
-  PRIMARY KEY (`id`),
-  KEY `user_index` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `feed`
---
-
-LOCK TABLES `feed` WRITE;
-/*!40000 ALTER TABLE `feed` DISABLE KEYS */;
-/*!40000 ALTER TABLE `feed` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `message`
---
-
-DROP TABLE IF EXISTS `message`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `message` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `from_id` int(10) unsigned DEFAULT NULL,
-  `to_id` int(10) unsigned DEFAULT NULL,
-  `content` text,
-  `create_at` datetime DEFAULT NULL,
-  `has_read` tinyint(1) DEFAULT NULL,
-  `image_url` varchar(255) DEFAULT '',
-  `conversation_id` varchar(128) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `message`
---
-
-LOCK TABLES `message` WRITE;
-/*!40000 ALTER TABLE `message` DISABLE KEYS */;
-INSERT INTO `message` VALUES (1,17,19,'hello','2020-10-20 03:28:11',1,'17-19',''),(2,17,19,'hello again','2020-10-20 03:28:53',1,'17-19',''),(3,17,20,'hello first','2020-10-20 03:47:29',0,'17-20',''),(4,22,17,'hello first','2020-10-20 03:49:55',0,'17-22','');
-/*!40000 ALTER TABLE `message` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `question`
@@ -152,6 +60,90 @@ CREATE TABLE `question` (
 ) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+
+--
+-- Table structure for table `message`
+--
+
+DROP TABLE IF EXISTS `message`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `message` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `from_id` int(10) unsigned DEFAULT NULL,
+  `to_id` int(10) unsigned DEFAULT NULL,
+  `content` text,
+  `create_at` datetime DEFAULT NULL,
+  `has_read` tinyint(1) DEFAULT NULL,
+  `image_url` varchar(255) DEFAULT '',
+  `conversation_id` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comment` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `content` text,
+  `entity_id` int(11) NOT NULL,
+  `entity_type` int(11) NOT NULL,
+  `create_at` datetime DEFAULT NULL,
+  `user_id` int(11) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `comment_user_id_fk` (`user_id`),
+  CONSTRAINT `comment_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `contact_list`
+--
+
+DROP TABLE IF EXISTS `contact_list`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `contact_list` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `conversation_id` varchar(1238) NOT NULL,
+  `create_at` datetime NOT NULL,
+  `update_at` datetime NOT NULL,
+  `latest_message_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `date_index` (`create_at`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+
+--
+-- Table structure for table `feed`
+--
+
+DROP TABLE IF EXISTS `feed`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `feed` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `create_at` datetime DEFAULT NULL,
+  `data` tinytext,
+  PRIMARY KEY (`id`),
+  KEY `user_index` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `feed`
+--
+
+
+
 --
 -- Dumping data for table `question`
 --
@@ -161,33 +153,8 @@ LOCK TABLES `question` WRITE;
 /*!40000 ALTER TABLE `question` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(64) NOT NULL,
-  `password` varchar(128) NOT NULL,
-  `salt` varchar(32) NOT NULL,
-  `head_url` varchar(256) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `index2` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `user`
---
-
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (17,'jiaweiw1','27487CD84C440534F2773997DA010815','e944b','www.baidu.com'),(18,'jiawei123','AA7A6AD7CA50385CF239A0EEC0B468E0','25f99','www.baidu.com'),(19,'yzy','43FE6D90EBF3D3EC2BDEF6600F47C380','eaa70','www.baidu.com'),(20,'yzy2','4088AEA98769FACFF75A8E0A2F177C14','5fd6f','www.baidu.com'),(21,'yzy3','F4428E356359E126A71D8CD9F1A8AE75','a8cf4','www.baidu.com'),(22,'wjw','893C60ECBE5C791EFD092D54DAF626A9','81c50','www.baidu.com');
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -199,3 +166,54 @@ UNLOCK TABLES;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2020-10-20  4:42:53
+
+
+
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (17,'jiaweiw1','27487CD84C440534F2773997DA010815','e944b','www.baidu.com'),(18,'jiawei123','AA7A6AD7CA50385CF239A0EEC0B468E0','25f99','www.baidu.com'),(19,'yzy','43FE6D90EBF3D3EC2BDEF6600F47C380','eaa70','www.baidu.com'),(20,'yzy2','4088AEA98769FACFF75A8E0A2F177C14','5fd6f','www.baidu.com'),(21,'yzy3','F4428E356359E126A71D8CD9F1A8AE75','a8cf4','www.baidu.com'),(22,'wjw','893C60ECBE5C791EFD092D54DAF626A9','81c50','www.baidu.com');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+--
+-- Dumping data for table `message`
+--
+
+LOCK TABLES `message` WRITE;
+/*!40000 ALTER TABLE `message` DISABLE KEYS */;
+INSERT INTO `message` VALUES (1,17,19,'hello','2020-10-20 03:28:11',1,'17-19',''),(2,17,19,'hello again','2020-10-20 03:28:53',1,'17-19',''),(3,17,20,'hello first','2020-10-20 03:47:29',0,'17-20',''),(4,22,17,'hello first','2020-10-20 03:49:55',0,'17-22','');
+/*!40000 ALTER TABLE `message` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `comment`
+--
+
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+INSERT INTO `comment` VALUES (24,'this is a comment 11',4,0,'2020-10-18 23:40:01',17),(25,'this is a comment 11',5,0,'2020-10-19 18:00:53',21);
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+--
+-- Dumping data for table `contact_list`
+--
+
+LOCK TABLES `contact_list` WRITE;
+/*!40000 ALTER TABLE `contact_list` DISABLE KEYS */;
+INSERT INTO `contact_list` VALUES (1,'17-19','2020-10-20 03:28:26','2020-10-20 03:29:12',2),(2,'17-20','2020-10-20 03:47:30','2020-10-20 03:47:30',3),(3,'17-22','2020-10-20 03:49:56','2020-10-20 03:49:56',4);
+/*!40000 ALTER TABLE `contact_list` ENABLE KEYS */;
+UNLOCK TABLES;
+
+LOCK TABLES `feed` WRITE;
+/*!40000 ALTER TABLE `feed` DISABLE KEYS */;
+/*!40000 ALTER TABLE `feed` ENABLE KEYS */;
+UNLOCK TABLES;
--- src/main/java/touch/forum/controller/QuestionController.java
+++ src/main/java/touch/forum/controller/QuestionController.java
@@ -38,6 +38,4 @@ public class QuestionController {
         return ResponseUtil.makeSuccessResponse(question);
     }
 
-
-
 }
